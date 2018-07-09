6d4e636c77978b646a789912986256d7d639c03a
3RebeccaSila3 <silarebecca26@gmail.com>
2018-07-09 11:46:03 +0300

    first commit

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main( String[]args) {
+        System.out.println( "Rebecca Sila");
+        System.out.println("Today's date is july 3 2018");
+    }
+}
